7b104da088e1c6c4594fb82c6e52c983eee2f070
anuradha1698 <anuradha.u.maurya@gmail.com>
2024-11-26 22:49:55 +0530

    receptionist dashboard overview, login and registration pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './App.module.scss';
-import Login from './pages/login';
-import PasswordReset from './pages/passwordReset';
-import RecoveryLink from './pages/recoveryLink';
-import UpdatePassword from './pages/updatePassword';
+import Login from './pages/login/login';
+import PasswordReset from './pages/login/passwordReset';
+import RecoveryLink from './pages/login/recoveryLink';
+import UpdatePassword from './pages/login/updatePassword';
 import Logo from './components/Logo';
+import Receptionist from './pages/receptionist'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-
+  const [isSignUpOrLogin, setIsSignUpOrLogin] = useState(true);
+  const [entity, setEntity] = useState("");
   return (
     <>
-      <Logo/>
-        <div className={`${styles.register}`}>
-          {/* <Login/> */}
-          {/* <PasswordReset/> */}
-          {/* <RecoveryLink/> */}
-          <UpdatePassword/>
+      <div className={`${isSignUpOrLogin ? "" : styles.crmApp}`}>
+        <div className={styles.logo}>
+        <Logo/>
         </div>
+        {/* if login or register page is actiive, add this class ${styles.loginPageActive} */}
+        <div className={`${styles.register} ${isSignUpOrLogin ? styles.loginPageActive : styles.otherPages}`}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Login setIsSignUpOrLogin={setIsSignUpOrLogin} />} />
+              <Route path="/password-reset" element={<PasswordReset setIsSignUpOrLogin={setIsSignUpOrLogin} />} />
+              <Route path="/recovery-link" element={<RecoveryLink setIsSignUpOrLogin={setIsSignUpOrLogin} />} />
+              <Route path="/update-password" element={<UpdatePassword setIsSignUpOrLogin={setIsSignUpOrLogin} />} />
+              <Route path="/receptionist" element={<Receptionist setIsSignUpOrLogin={setIsSignUpOrLogin} setEntity={setEntity} entity={entity}/>} />
+            </Routes>
+          </Router>
+        </div>
+      </div>
     </>
   );
 }
